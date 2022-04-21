07c623ebecc811361a2559bdae8490485776d0f3
MUSKANAGRAWAL81 <95308310+MUSKANAGRAWAL81@users.noreply.github.com>
2022-04-21 10:50:03 +0530

    Add files via upload

---
+++ LINKEDKI.C
@@ -0,0 +1,234 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+// define structure
+typedef struct nodetype
+{
+int info;
+struct nodetype*next;
+}node;
+// function prototype
+void createEmptyList(node **head);
+void TraverseInOrder(node *head);
+void InsertAtBegining(node **head,int item);
+void traverseInReverse(node *head);
+void InsertAtEnd(node **head,int item);
+void InsertAfterElement(node *head,int item,int after);
+node *Search(node *head,int element);
+void deleteFromBegining(node **head);
+void deleteFromEnd(node **head);
+void deleteAfterElement(node *head,int after);
+
+void main()
+{
+node*head;
+int Choice,element,after,res;
+clrscr();
+createEmptyList(&head);
+while(1)
+{
+printf("Operations Available are\n");
+printf("1. Insert at begining\n");
+printf("2. Traverse in order\n");
+printf("3.Traverse in reverse\n");
+printf("4. insert at the end\n");
+printf("5.insert after the postion\n");
+printf("6.search any element\n");
+printf("7.delete element from begining\n");
+printf("8. delete element from end\n");
+printf("9.delete after the postion\n");
+printf("enter the choice:\n" );
+scanf("%d",&Choice);
+switch(Choice)
+{
+case 1: printf("\nEnter the element");
+scanf("%d",&element);
+InsertAtBegining(&head,element);
+break;
+case 2: if(head==NULL)
+			   printf("\n List is empty");
+		else
+
+		TraverseInOrder(head);
+		printf("\n Pass any key to continue");
+		getch();
+		break;
+case 3: if(head==NULL)
+			  printf("\n List is empty");
+		else
+		   traverseInReverse(head);
+		   printf("\npass any key to continue");
+		   getch();
+		   break;
+case 4: printf("enter the element to be insert:\n");
+		scanf("%d",&element);
+		InsertAtEnd(&head, element);
+		break;
+case 5: printf("enter the position\n");
+		scanf("%d",&after);
+		printf("enter the element");
+		scanf("%d",&element);
+		InsertAfterElement(head ,element,after);
+		break;
+case 6: printf("Enter the element\n");
+		scanf("%d",&element);
+		res=(int)Search(head, element);
+		printf("%d",res);
+		break;
+case 7: deleteFromBegining( &head);
+		   printf("Element deleted\n");
+		   printf("press any key to continue\n");
+		   getch();
+		   break;
+case 8: deleteFromEnd(&head);
+		   printf("element deleted\n");
+		   printf("press any key to continue\n");
+		   getch();
+		   break;
+case 9: printf("enter the postion:\n");
+		scanf("%d",&after);
+		deleteAfterElement(head,after);
+		printf("press any key to continue:\n");
+		getch();
+		break;
+default: exit(0);
+		}
+	  }
+
+}
+void createEmptyList(node **head )
+{
+*head=NULL;
+}
+void InsertAtBegining(node **head,int item)
+{
+node *ptr;
+ptr= (node*)malloc(sizeof(node));
+ptr->info=item;
+if(*head==NULL)
+ptr->next=NULL;
+else
+ptr->next= *head;
+*head= ptr;
+
+}
+void TraverseInOrder(node *head)
+{
+while(head!=NULL)
+{
+printf("\n %d",head->info);
+head= head->next;
+}
+}
+
+void traverseInReverse(node *head)
+{
+if(head->next!=NULL)
+traverseInReverse(head->next);
+printf("%d",head->info);
+}
+void InsertAtEnd(node **head, int item)
+{
+node *ptr,*loc;
+ptr =(node *)malloc(sizeof(node));
+ptr->info=item;
+ptr->next=NULL;
+if(*head==NULL)
+{
+*head=ptr;
+}
+else{
+loc = *head;
+while(loc->next!=NULL)
+{
+loc=loc->next;
+}
+loc->next=ptr;
+}
+}
+void InsertAfterElement(node *head, int item, int after)
+{
+node *ptr,*loc;
+loc=Search(head,after);
+if(loc==(node *)NULL)
+return;
+else
+{
+ptr=(node*)malloc(sizeof(node));
+ptr->info=item;
+ptr->next=loc->next;
+loc->next=ptr;
+}}
+
+node *Search(node *head ,int element)
+{
+while((head!=NULL)&&(head->info!= element))
+head=head->next;
+return head;
+}
+void deleteFromBegining(node **head)
+{
+node *ptr;
+if(*head==NULL)
+return;
+else
+{
+ptr= *head;
+*head =(*head)->next;
+free(ptr);
+}
+}
+void deleteFromEnd(node **head)
+{
+node *ptr,*loc;
+if(*head==NULL)
+return;
+else if((*head)->next== NULL)
+{
+ptr =*head;
+*head=NULL;
+free(ptr);
+}
+else
+{
+loc=*head;
+ptr=(*head)->next;
+while(ptr->next!=NULL)
+{
+loc=ptr;
+ptr=ptr->next;
+}
+loc->next=NULL;
+free(ptr);
+}
+}
+void deleteAfterElement(node *head,int after)
+{
+node *ptr,*loc;
+loc=Search(head,after);
+if(loc==(node*)NULL)
+return;
+
+ptr=loc->next;
+loc->next=ptr->next;
+free(ptr);
+}
+void reverseList(node **head)
+{
+node *previousNode, *currentNode, *nextNode;
+currentNOde=*head;
+nextNode=currentNode->next;
+previousNode=NULL;
+currentNode->next=NULL;
+while(nextNode!=NULL)
+{
+previousNode=currentNode;
+currentNode=nextNode;
+nextNode=current->next;
+currentNode->next=previousNode;
+}
+*head=currentNode;
+}
+
+
+
