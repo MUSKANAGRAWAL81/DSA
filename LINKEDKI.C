db91c1198c40d4decefa939ae842c26e8991aae5
MUSKANAGRAWAL81 <95308310+MUSKANAGRAWAL81@users.noreply.github.com>
2022-04-28 10:17:43 +0530

    Add files via upload

--- LINKEDKI.C
+++ LINKEDKI.C
@@ -18,7 +18,9 @@ node *Search(node *head,int element);
 void deleteFromBegining(node **head);
 void deleteFromEnd(node **head);
 void deleteAfterElement(node *head,int after);
-
+void reverseList(node **head);
+void deleteList(node **head);
+node *SearchInSorted(node *head, int item);
 void main()
 {
 node*head;
@@ -37,6 +39,9 @@ printf("6.search any element\n");
 printf("7.delete element from begining\n");
 printf("8. delete element from end\n");
 printf("9.delete after the postion\n");
+printf("10.reverse the linked list\n");
+printf("11. delete the whole linked list\n");
+printf("12.search an element in asorted linked list\n");
 printf("enter the choice:\n" );
 scanf("%d",&Choice);
 switch(Choice)
@@ -73,7 +78,7 @@ case 5: printf("enter the position\n");
 case 6: printf("Enter the element\n");
 		scanf("%d",&element);
 		res=(int)Search(head, element);
-		printf("%d",res);
+		printf("%d\n",res);
 		break;
 case 7: deleteFromBegining( &head);
 		   printf("Element deleted\n");
@@ -91,6 +96,21 @@ case 9: printf("enter the postion:\n");
 		printf("press any key to continue:\n");
 		getch();
 		break;
+case 10:reverseList(&head);
+		printf("linkelist reversed.\n");
+		printf("press any key to continue:\n");
+		getch();
+		break;
+case 11: deleteList(&head);
+		 printf("Linked list deleted\n");
+		 printf("press any key to continue\n");
+		 getch();
+		 break;
+case 12: printf("enter the element to be searched:\n");
+		 scanf("%d",&element);
+		 res =(int)SearchInSorted(head,element);
+		 printf("%d\n",res);
+		 break;
 default: exit(0);
 		}
 	  }
@@ -216,7 +236,7 @@ free(ptr);
 void reverseList(node **head)
 {
 node *previousNode, *currentNode, *nextNode;
-currentNOde=*head;
+currentNode=*head;
 nextNode=currentNode->next;
 previousNode=NULL;
 currentNode->next=NULL;
@@ -224,10 +244,33 @@ while(nextNode!=NULL)
 {
 previousNode=currentNode;
 currentNode=nextNode;
-nextNode=current->next;
+nextNode=currentNode->next;
 currentNode->next=previousNode;
 }
 *head=currentNode;
+}
+void deleteList(node **head)
+{
+node *ptr;
+while(*head!=NULL)
+{
+ptr= *head;
+*head =(*head)->next;
+free(ptr);
+}
+}
+node *SearchInSorted(node *head,int item)
+{
+while(head!=NULL)
+{
+if(item<head->info)
+return NULL;
+else if(head->info==item)
+return head;
+else
+head=head->next;
+}
+return NULL;
 }
 
 
